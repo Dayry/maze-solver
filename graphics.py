7e831add890118a3c558cc5c1e72fbf5eb0d78a5
Ryan <dayry529@gmail.com>
2022-12-29 10:16:36 +1300

    add maze class still working on bugs

--- graphics.py
+++ graphics.py
@@ -88,5 +88,55 @@ class Cell:
         movement = Line(self_center, to_cell_center)
         self.__win.draw_line(movement, line_colour)
 
+    
+class Maze:
+
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win):
+        self.__x1 = x1,
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+
+        self.__create_cells()
+
+    def __create_cells(self):
+        for r in range(self.__num_rows):
+            col = []
+            for c in range(self.__num_cols):
+                x1, y1, x2, y2, = self.__calc_pos(self.__x1, self.__y1, self.__cell_size_x, self.__cell_size_y, r, c)
+                #col[c] = Cell(x1, y1, x2, y2, self.__win)
+                col.append(Cell(x1, y1, x2, y2, self.__win))
+            self.__cells.append(col)
+        
+        for row in self.__cells:
+            for cell in row:
+                cell.draw()
+
+
+    def __calc_pos(
+            self, maze_x, maze_y,
+            cell_size_x, cell_size_y,
+            cell_row, cell_col):
+        x1 = cell_size_y * (cell_row + 1) - cell_size_y # these are so dirty
+        y1 = cell_size_x * (cell_col + 1) - cell_size_x
+        x2 = x1 + cell_size_x
+        y2 = y1 + cell_size_y
+
+        return x1, y1, x2, y2
+    
+
+    
         
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from graphics import Window, Line, Point, Cell
+from graphics import Window, Line, Point, Cell, Maze
 
 
 def main():
@@ -10,17 +10,18 @@ def main():
     line_a = Line(point_1, point_2)
     win.draw_line(line_a, "black")"""
 
-    # Cell tests
+    """# Cell tests
     cell1 = Cell(350, 300, 400, 350, win)
     cell1.draw()
     cell2 = Cell(550, 150, 600, 200, win)
     cell2.draw()
-
-    cell3 = Cell(10, 200, 100, 30, win)
+    cell3 = Cell(10, 10, 400, 300, win)
     cell3.draw()
-
     cell1.draw_move(cell2)
-    cell2.draw_move(cell3, True)
+    cell2.draw_move(cell3, True)"""
+
+    # Maze test
+    maze = Maze(10, 10, 6, 4, 60, 50, win)
 
 
     win.wait_for_close()
