b0b4b282a417872278585e010b720bfc10b4a1b6
Ryan <dayry529@gmail.com>
2022-12-31 10:16:22 +1300

    added a function that loops over all cells and calls the recursive break_walls function

--- maze.py
+++ maze.py
@@ -29,7 +29,7 @@ class Maze:
 
         self._create_cells()
         self._break_entrance_and_exit()
-        self._break_walls_r(0, 0) # start in entrance cell
+        self._break_walls()
 
     def _create_cells(self):
         for r in range(self._num_cols):
@@ -46,8 +46,8 @@ class Maze:
                 for cell in col:
                     cell.draw()
                     self._animate()
-
-
+    
+    """ Helper method for _create_cells """
     def _calc_pos(
             self, maze_x, maze_y,
             cell_size_x, cell_size_y,
@@ -59,7 +59,6 @@ class Maze:
         y2 = y1 + cell_size_y
 
         return x1, y1, x2, y2
-
 
     def _animate(self):
         self._win.redraw()
@@ -73,6 +72,16 @@ class Maze:
         exit_cell.has_bottom_wall = False
         exit_cell.draw()
 
+    """ 
+    Create a path starting from every cell until no more paths can be made
+    """
+    def _break_walls(self):
+        for col in range(self._num_cols):
+            for cell in range(self._num_rows):
+                self._break_walls_r(col, cell)
+                
+
+    """ Helper method for _break_walls """
     def _break_walls_r(self, x, y): # x is col, y is index in given col
         self._cells[x][y].visited = True
         possible_paths = []
@@ -89,30 +98,34 @@ class Maze:
             next_x = possible_paths[rand][0]
             next_y = possible_paths[rand][1]
             next_pos = possible_paths[rand][2]
+            
+            self._draw_wall_break(x, y, next_x, next_y, next_pos)
+            self._break_walls_r(next_x, next_y)       
 
-            if next_pos == 0: # next cell is above
-                self._cells[x][y].has_top_wall = False
-                self._cells[x][y].draw()
-                self._cells[next_x][next_y].has_bottom_wall = False
-                self._cells[next_x][next_y].draw()
-            if next_pos == 1: # next cell is below
-                self._cells[x][y].has_bottom_wall = False
-                self._cells[x][y].draw()
-                self._cells[next_x][next_y].has_top_wall = False
-                self._cells[next_x][next_y].draw()
-            if next_pos == 2: # next cell is left
-                self._cells[x][y].has_left_wall = False
-                self._cells[x][y].draw()
-                self._cells[next_x][next_y].has_right_wall = False
-                self._cells[next_x][next_y].draw()
-            if next_pos == 3: # next cell is right
-                self._cells[x][y].has_right_wall = False
-                self._cells[x][y].draw()
-                self._cells[next_x][next_y].has_left_wall = False
-                self._cells[next_x][next_y].draw()
+    """ Helper method for _break_walls_r """
+    def _draw_wall_break(self, x, y, next_x, next_y, next_pos):
+        if next_pos == 0: # next cell is above
+            self._cells[x][y].has_top_wall = False
+            self._cells[x][y].draw()
+            self._cells[next_x][next_y].has_bottom_wall = False
+            self._cells[next_x][next_y].draw()
+        if next_pos == 1: # next cell is below
+            self._cells[x][y].has_bottom_wall = False
+            self._cells[x][y].draw()
+            self._cells[next_x][next_y].has_top_wall = False
+            self._cells[next_x][next_y].draw()
+        if next_pos == 2: # next cell is left
+            self._cells[x][y].has_left_wall = False
+            self._cells[x][y].draw()
+            self._cells[next_x][next_y].has_right_wall = False
+            self._cells[next_x][next_y].draw()
+        if next_pos == 3: # next cell is right
+            self._cells[x][y].has_right_wall = False
+            self._cells[x][y].draw()
+            self._cells[next_x][next_y].has_left_wall = False
+            self._cells[next_x][next_y].draw()
 
-            self._break_walls_r(next_x, next_y)
-    
+    """ Helper method for _break_walls_r """
     def _find_adjacent_cells(self, x, y):
         adjacent_cells = [None, None, None, None]
         if y - 1 >= 0: # top
